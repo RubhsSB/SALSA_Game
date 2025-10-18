34418623d6862735918cc90d3054f30f8c576b95
RubhsSB <rhs5656@gmail.com>
2025-10-18 17:21:11 +0200

    Update console log messages in service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-// sw.js
 self.addEventListener("install", event => {
-  console.log("✅ Service Worker instalado");
+  console.log("Service Worker instalado.");
 });
 
 self.addEventListener("fetch", event => {
-  // Aquí puedes manejar caché si lo deseas
+  // Puedes agregar lógica de caché si lo deseas
 });
